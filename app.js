4ff4db7a07b24ba3588c4ef743b80708f4a22a2b
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-07-26 16:54:25 +0530

    commit

--- app.js
+++ app.js
@@ -1,48 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-var bodyParser = require('body-parser')
+
 require("dotenv").config();
 const mongoose = require("mongoose");
 //const cors = require("cors");
- 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //require
-const user = require("./routes/user");
-//const staff = require("./routes/staff");
  
+const user = require("./routes/user")
 
  
- 
+
+
+
+
+
 
 //use
 app.use("/", user);
-app.use(express.json)
+
+
 app.get("/", (req, res) => {
   res.send("Hello World!!!!");
 });
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-  
-    // render the error page
-    res.status(err.status || 5000);
-    res.render("error");
-  });
+
 //console.log(process.env.DATABASE);
 mongoose
   .connect(process.env.DB, {
     useNewUrlParser: true,
     // useFindAndModify: false,
-    useNewUrlParser: true,                 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
     //useFindAndModify: false,
   })
@@ -57,4 +49,4 @@ app.listen(process.env.PORT || 8000, () => {
   console.log("Example app listening on port 8000");
 });
 
-//    http://localhost:9000/admin
+//    http://localhost:5000/admin
