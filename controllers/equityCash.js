051a288fa67608c810ee76a2ef71162fe6049b72
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-08-09 17:38:19 +0530

    comit

--- controllers/equityCash.js
+++ controllers/equityCash.js
@@ -1,75 +1,74 @@
 const EquityCash = require("../models/equityCash");
 const resp = require("../helpers/apiResponse");
 
-exports.add_equityCash   = async (req, res) => {
-    
-    const { script_type,script_name,active_value,active_value2,call_type,SL,sl_type,T1,t1_type,T2,t2_type,T3,t3_type,T4,t4_type,qty,investment_amt,no_of_lots,pl_type,profit_loss } = req.body;
+exports.add_equityCash = async (req, res) => {
+
+  const { script_type, script_name, active_value, active_value2, call_type, SL, sl_type, T1, t1_type, T2, t2_type, T3, t3_type, T4, t4_type, qty, investment_amt, no_of_lots, pl_type, profit_loss } = req.body;
 
 
-  
+
   const newEquityCash = new EquityCash({
-    
-    script_type:script_type,
-    equity_script: equity_script,
-    script_name:script_name,
-    active_value:active_value,
-    active_value2:active_value2,
-    call_type:call_type,
-    SL:SL,
-    sl_type:sl_type,
-    T1:T1,
-    t1_type:t1_type,
-    T2:T2,
-    t2_type:t2_type,
-    T3:T3,
-    t3_type:t3_type,
-    T4:T4,
-    t5:t5,
-    t5_type:t5_type,
-    qty:qty,
-    investment_amt:investment_amt,
-    no_of_lots:no_of_lots,
-    pl_type:pl_type,
-    profit_loss:profit_loss,
-    expiryDate:expiryDate
- 
+
+    script_type: script_type,
+    script_name: script_name,
+    active_value: active_value,
+    active_value2: active_value2,
+    call_type: call_type,
+    SL: SL,
+    sl_type: sl_type,
+    T1: T1,
+    t1_type: t1_type,
+    T2: T2,
+    t2_type: t2_type,
+    T3: T3,
+    t3_type: t3_type,
+    T4: T4,
+    t5: t5,
+    t5_type: t5_type,
+    qty: qty,
+    investment_amt: investment_amt,
+    no_of_lots: no_of_lots,
+    pl_type: pl_type,
+    profit_loss: profit_loss,
+    expiryDate: expiryDate
+
   });
- 
+
   newEquityCash
-      .save()
-      .then((data) => resp.successr(res, data))
-      .catch((error) => resp.errorr(res, error));
-  }
+    .save()
+    .then((data) => resp.successr(res, data))
+    .catch((error) => resp.errorr(res, error));
+}
 
 
 
 exports.equityCash_list = async (req, res) => {
-    await EquityCash.find().populate("script_name")
-      .sort({ sortorder: 1 })
-      .then((data) => resp.successr(res, data))
-      .catch((error) => resp.errorr(res, error));
-  };
+  await EquityCash.find().populate("script_name")
+    .sort({ sortorder: 1 })
+    .then((data) => resp.successr(res, data))
+    .catch((error) => resp.errorr(res, error));
+};
 
-  exports.getone_equityCash = async (req, res) => {
-    await EquityCash.findOne({ _id: req.params.id }).populate("script_name")
-      .then((data) => resp.successr(res, data))
-      .catch((error) => resp.errorr(res, error));
-  };
+exports.getone_equityCash = async (req, res) => {
+  await EquityCash.findOne({ _id: req.params.id }).populate("script_name")
+    .then((data) => resp.successr(res, data))
+    .catch((error) => resp.errorr(res, error));
+};
 
-  exports.edit_equityCash = async (req, res) => {
-    await EquityCash.findOneAndUpdate(
-      {
-        _id: req.params.id,
-      },
-      { $set: req.body },
-      { new: true }
-    )
-      .then((data) => resp.successr(res, data))
-      .catch((error) => resp.errorr(res, error));
-  };
-  
-  exports.dlt_equityCash = async (req, res) => {
-    await EquityCash.deleteOne({ _id: req.params.id })
-      .then((data) => resp.deleter(res, data))
-      .catch((error) => resp.errorr(res, error));
-  };
+exports.edit_equityCash = async (req, res) => {
+  await EquityCash.findOneAndUpdate(
+    {
+      _id: req.params.id,
+    },
+    { $set: req.body },
+    { new: true }
+  )
+    .then((data) => resp.successr(res, data))
+    .catch((error) => resp.errorr(res, error));
+};
+
+exports.dlt_equityCash = async (req, res) => {
+  await EquityCash.deleteOne({ _id: req.params.id })
+    .then((data) => resp.deleter(res, data))
+    .catch((error) => resp.errorr(res, error));
+};
